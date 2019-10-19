77606caadd12b55625b65f392cb6b60b2d0cec3e
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2019-10-19 15:42:35 +0000

    Move to the Jenkins shared libraries + docker build from tln

--- service.go
+++ service.go
@@ -32,7 +32,7 @@ func main() {
 
   srv := &http.Server{
     Handler:      r,
-    Addr:         ":8080",
+    Addr:         ":9080",
     ReadTimeout:  10 * time.Second,
     WriteTimeout: 10 * time.Second,
   }
